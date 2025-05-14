725c7ed2cc4da5bb010fc37cfd07138682426b1a
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-14 12:02:05 -0600

    basic design for site complete

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+// what do i want to do? 
+// i want the user to come to the site... click month button and a pop-up appears asking for user to enter the month, then, when submitted, it'll appear on the screen under month- do the same for day and year... once finished, it'll calculate everything and give the actual age
+
+// I - user enters month, day, year
+
+// P - function to whenClickedMonth()- prompt
+
+// O - 
