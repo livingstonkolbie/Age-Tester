afdba51ac6fe56435cfebe1354f2e45c15c2578d
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-14 14:23:38 -0600

    working on it
    need a little break

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 
 // what do i want to do? 
-// i want the user to come to the site... click month button and a pop-up appears asking for user to enter the month, then, when submitted, it'll appear on the screen under month- do the same for day and year... once finished, it'll calculate everything and give the actual age
+
 
 // I - user enters month, day, year
 
-// P - function to whenClickedMonth()- prompt
+// P 
+// function displayAge()...
+// saves the selections
+// knows todays date
+// 2025 minus the year they choose....
+// when they click the question mark, that function happens
 
-// O - 
+// O - display the number age... if they choose 1925 put a dinosaur... if they choose a future date, put a fetus
+
+
+
+// Year Function
+document.getElementById('yearRange').addEventListener('input', function() {
+    document.getElementById('yearDisplay').textContent = this.value;
+});
+
+
+function displayAge() {
+    // when the user clicks one of the months, it saves that value here
+    let saveMonth = document 
+
+
+    let clickQuestionMark = document.querySelector
+}
